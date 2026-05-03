a7231037212b5c86d9468d6a1ac5f7087e529fb9
ShreyanshR <shreyanshrathore31@gmail.com>
2026-05-03 23:09:21 +0200

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> stk;
+        std::unordered_map<char, char> closeToOpen{
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        for(auto& c: s){
+            if(closeToOpen.count(c) && !stk.empty() && closeToOpen[c] == stk.top()){
+                stk.pop();
+            } else{
+                stk.push(c);
+            }
+            
+        }
+
+        return stk.empty();
+    }
+};
